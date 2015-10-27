798390c155d88e2c227d45e6a5284c0ea4659eac
Daisuke Fujita <dtanshi45@gmail.com>
2015-10-27 20:01:19 +0900

    Implement terminate command

--- command/terminate.go
+++ command/terminate.go
@@ -1,7 +1,12 @@
 package command
 
 import (
+	"flag"
+	"fmt"
 	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
 type TerminateCommand struct {
@@ -9,7 +14,48 @@ type TerminateCommand struct {
 }
 
 func (c *TerminateCommand) Run(args []string) int {
-	// Write your code here
+	var (
+		dryRun           bool
+		instanceIdstring string
+		instanceIds      []*string
+	)
+
+	var (
+		arguments []string
+	)
+
+	svc := ec2.New(&aws.Config{})
+
+	flags := flag.NewFlagSet("dtan4", flag.ContinueOnError)
+	flags.Usage = func() {}
+
+	flags.BoolVar(&dryRun, "dry-run", false, "Dry run (default: false)")
+	flags.StringVar(&instanceIdstring, "instance", "", "Instance IDs")
+
+	if err := flags.Parse(args[0:]); err != nil {
+		return 1
+	}
+
+	for 0 < flags.NArg() {
+		arguments = append(arguments, flags.Arg(0))
+		flags.Parse(flags.Args()[1:])
+	}
+
+	for _, id := range strings.Split(instanceIdstring, ",") {
+		instanceIds = append(instanceIds, aws.String(id))
+	}
+
+	opts := &ec2.TerminateInstancesInput{
+		DryRun:      aws.Bool(dryRun),
+		InstanceIds: instanceIds,
+	}
+
+	resp, err := svc.TerminateInstances(opts)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
 
 	return 0
 }
